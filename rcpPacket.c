7fdee0309a83cd1d8eda08b68dd8c0e278a99eb8
Jay Reynolds <reynoldsjay@gatech.edu>
2017-04-12 13:03:37 -0400

    Packet methods in C except packetize.

--- rcpPacket.c
+++ rcpPacket.c
@@ -1,34 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef char bool;
 
-char* createPacket(int syn, int ack, int seq, int dataSize, char[] data) {
-    char flagByte;
-    if (syn && ack) {
-        flagByte = 3;
-    } else if (syn) {
-        flagByte = 2;
-    } else if (ack) {
-        flagByte = 1;
-    } else {
-        flagByte = 0;
-    }
-    char* packet = malloc(sizeof(char) + sizeof(int) + sizeof(int) + dataSize);
-    packet[0] = flagByte;
+typedef struct {
+    bool syn;
+    bool ack;
+    int seq;
+    int dataSize;
+    char* data;
+} Packet;
 
-    char* pSeq = (char*) &seq;
-    copyInt(packet, 1, pSeq);
-
-    char* pDS = (char*) &dataSize;
-    copyInt(packet, 1 + sizeof(int), pDS);
-
-    for (int i = 0; i < dataSize; i++) {
-        packet[i + 1 + sizeof(int)*2] = data[i]
-    }
+Packet* createPacket(bool syn, bool ack, int seq, int dataSize, char data[]) {
+    Packet* packet = (Packet *)malloc(sizeof(Packet));
+    // copy info into packet
+    packet->syn = syn;
+    packet->ack = ack;
+    packet->seq = seq;
+    packet->dataSize = dataSize;
+    packet->data = data;
     return packet;
 }
 
-void copyInt(char* pChar, int startingCharInd, char* pInt) {
-    for (int i = 0; i < sizeof(int); i++) {
-        pChar[i+startingCharInd] = pInt[i];
+bool isAck(Packet* packet) {
+    return packet->ack;
+}
+
+bool isSyn(Packet* packet) {
+    return packet->syn;
+}
+
+bool isSynAck(Packet* packet) {
+    if (packet->ack && packet->syn) {
+        return 1;
     }
+    return 0;
+}
+
+int extractSeq(Packet* packet) {
+    return packet->seq;
+}
+
+char* extractData(Packet* packet) {
+    return packet->data;
+}
+
+int extractDataSize(Packet* packet) {
+    return packet->dataSize;
+}
+
+bool isData(Packet* packet) {
+    return packet->dataSize > 0;
+}
+
+void destroyPacket(Packet* packet) {
+    free(packet->data);
+    free(packet);
+}
+
+// Packet** packetize(int startingSeqNum, char* data) {
+//
+// }
+
+int main() {
+    char data[5] = {'a', 'a', 'a', 'a', 'a'};
+    Packet* packet = createPacket(1, 0, 30, 5, data);
+    bool ack = isAck(packet);
+    bool syn = isSyn(packet);
+    bool synack = isSynAck(packet);
+    if (ack) {
+        printf("is ack\n");
+    }
+    if (!syn) {
+        printf("isn't syn\n");
+    }
+    printf("dataSize is %i\n", extractDataSize(packet));
+    printf("seq num is %i\n", extractSeq(packet));
 }

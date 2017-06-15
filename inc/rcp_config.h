6e4ca21a3dfe7c23d6043d851aaca7c53cff1029
Baijun <bdesai35@gatech.edu>
2017-06-15 00:29:46 -0700

    Updated debugging messages in rcp.c

--- inc/rcp_config.h
+++ inc/rcp_config.h
@@ -1,13 +1,15 @@
 #ifndef RCP_CONFIG_H
 #define RCP_CONFIG_H
 
-#include <stdlib.h>
-
 #ifndef DEBUG
+
 #define DEBUG 1 //Set this flag while debugging
 #endif
 
 #if DEBUG
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 #define DEBUG_PRINT(...) do{ fprintf( stderr, __VA_ARGS__ ); } while( false ) //Only works on C99 compiler
 #else
 #define DEBUG_PRINT(...) do{ } while ( false )
--- src/rcp.c
+++ src/rcp.c
@@ -4,7 +4,7 @@
 #include "rcp_config.h"
 
 int32_t rcp_socket(){
-    // DEBUG_PRINT("Creating UDP socket.\n");
+    DEBUG_PRINT("Creating UDP socket.\n");
     return (int32_t) socket(AF_INET, SOCK_DGRAM, 0);
 }
 
@@ -15,8 +15,9 @@ int32_t rcp_bind(int32_t fd, rcp_sockaddr_in *rcp_sock, uint16_t port){
     myaddr.sin_family = AF_INET;
     myaddr.sin_addr.s_addr = htonl(INADDR_ANY); //Let OS decide which IP address this port is assigned to
     myaddr.sin_port = htons(port); //The port that the socket binds to
-    // DEBUG_PRINT("Binding socket at %x:%hu\n", myaddr.sin_addr.s_addr, my_addr.sin_port);
-    return bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr));
+    int32_t b = bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr));
+    DEBUG_PRINT("Binding socket at %x:%hu\n", ntohl(myaddr.sin_addr.s_addr), ntohs(myaddr.sin_port));
+    return b;
 }
 
 int32_t rcp_close(int32_t fd){

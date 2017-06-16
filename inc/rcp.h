32c3caff3caa3ff1c7a25633825c2e2fec5f5563
Baijun <bdesai35@gatech.edu>
2017-06-16 09:08:45 -0700

    Added skeleton for rcp_send, rcp_send_packet, and rcp_receive_packet.

--- inc/rcp.h
+++ inc/rcp.h
@@ -5,9 +5,22 @@
 #include <stdint.h>
 #include <netinet/in.h> //for sockaddr_in
 
-typedef struct rcp_sockaddr_in{
+typedef struct {
     struct sockaddr_in addr;
 } rcp_sockaddr_in;
+
+
+typedef enum {
+    SEND_NO_ERROR,
+    SEND_NO_CONNECTION,
+    SEND_GENERIC_ERROR
+} RCP_Send_Error;
+
+typedef struct {
+    int32_t fd; //The file descriptor of the associated socket.
+    int32_t seq; //The outbound sequence number.
+    int32_t ack; //The outbound ack number.
+} rcp_connection;
 
 /**
  * Creates a UDP socket with same return values as socket in standard library
@@ -26,10 +39,21 @@ int32_t rcp_socket();
 int32_t rcp_bind(int32_t fd, rcp_sockaddr_in *rcp_sock, uint16_t port);
 
 /**
+ * SImilar to TCP send. Adds data to the send buffer for transmission.
+ * @param  rcp_conn The rcp_connection struct associated with the connection
+ * @param  buf      The message to be transmitted.
+ * @param  len      The length of the message.
+ * @return          Send error if one occured.
+ */
+RCP_Send_Error rcp_send(rcp_connection *rcp_conn, const void *buf, size_t len);
+
+/**
  * Closes socket
  * @param  fd File descriptor of socket
  * @return    Same return value as standard socket close
  */
 int32_t rcp_close(int32_t fd);
+
+
 
 #endif
--- src/rcp.c
+++ src/rcp.c
@@ -1,7 +1,17 @@
 #include <string.h>
 #include <unistd.h> //For close()
-#include "rcp.h"
 #include "rcp_config.h"
+#include "rcp.h"
+#include "rcp_packet.h"
+
+typedef enum {
+    PKT_NO_ERROR,
+    PKT_SEND_PACKET_ERROR,
+    PKT_RECEIVE_PACKET_ERROR
+} Packet_Error;
+
+static Packet_Error rcp_send_packet(rcp_connection *rcp_conn, Packet *packet);
+static Packet_Error rcp_receive_packet(rcp_connection *rcp_conn, Packet *packet);
 
 int32_t rcp_socket(){
     DEBUG_PRINT("Creating UDP socket.\n");
@@ -10,16 +20,30 @@ int32_t rcp_socket(){
 
 int32_t rcp_bind(int32_t fd, rcp_sockaddr_in *rcp_sock, uint16_t port){
     struct sockaddr_in myaddr = rcp_sock->addr;
-
     memset((char *)&myaddr, 0, sizeof(myaddr));
     myaddr.sin_family = AF_INET;
     myaddr.sin_addr.s_addr = htonl(INADDR_ANY); //Let OS decide which IP address this port is assigned to
     myaddr.sin_port = htons(port); //The port that the socket binds to
-    int32_t b = bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr));
     DEBUG_PRINT("Binding socket at %x:%hu\n", ntohl(myaddr.sin_addr.s_addr), ntohs(myaddr.sin_port));
+    int32_t b = bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr));
     return b;
 }
 
+
+RCP_Send_Error rcp_send(rcp_connection *rcp_conn, const void *buf, size_t len){
+    return SEND_NO_ERROR;
+}
+
+
 int32_t rcp_close(int32_t fd){
     return close(fd);
+}
+
+
+static Packet_Error rcp_send_packet(rcp_connection *rcp_conn, Packet *packet){
+    return PKT_NO_ERROR;
+}
+
+static Packet_Error rcp_receive_packet(rcp_connection *rcp_conn, Packet *packet){
+    return PKT_NO_ERROR;
 }
